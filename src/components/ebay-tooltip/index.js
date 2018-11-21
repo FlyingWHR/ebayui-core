0b263ec10797f76dd08a48d13c8f1345992834e1
Sean Gates <sgates@ebay.com>
2018-11-21 11:18:52 -0700

    Improvements; using makeup-expander;

--- src/components/ebay-tooltip/index.js
+++ src/components/ebay-tooltip/index.js
@@ -1,12 +1,15 @@
+const Expander = require('makeup-expander');
 const processHtmlAttributes = require('../../common/html-attributes');
 const template = require('./template.marko');
 
 function getInitialState(input) {
+    const { style, open = false, type } = input;
     return {
         htmlAttributes: processHtmlAttributes(input),
         class: [`tooltip`, input.class],
-        style: input.style,
-        type: input.type
+        style,
+        type,
+        open
     };
 }
 
@@ -14,8 +17,20 @@ function getTemplateData(state) {
     return state;
 }
 
+function init() {
+    const expander = new Expander(this.el, { // eslint-disable-line no-unused-vars
+        hostSelector: '.tooltip__host',
+        contentSelector: '.tooltip__overlay',
+        focusManagement: 'focusable',
+        expandOnFocus: true,
+        expandOnHover: true,
+        autoCollapse: true
+    });
+}
+
 module.exports = require('marko-widgets').defineComponent({
     template,
     getInitialState,
-    getTemplateData
+    getTemplateData,
+    init
 });
